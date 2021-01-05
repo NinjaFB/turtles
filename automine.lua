e858ac25705ae3f185cedd334cbdeda9eee3ddc3
NinjaFB <kiwironbergs@gmail.com>
2021-01-05 18:43:22 +1100

    Update automine.lua

--- automine.lua
+++ automine.lua
@@ -2,6 +2,15 @@ local bl_bool, bl_str = turtle.inspectDown()
 
 while(bl_str ~= "minecraft:bedrock")
 do
-    turtle.digDown()
+    if(bl_str == "minecraft:lava" or "minecraft:water" or "minecraft:flowing_lava" or "minecraft:flowing_water")
+    then
+        for i = 1, 4, 1
+        do
+            if(turtle.detect())
+            do
+                turtle.place()
+            end
+            turtle.turnLeft()
+        end
     turtle.down()
 end
